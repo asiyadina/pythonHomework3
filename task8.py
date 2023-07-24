a712ac587828c1abfb140e7620027bad4bb5aa61
«Smirnova <>
2023-07-24 15:36:33 +0300

    решение 8 задачи ,что не успели на семинаре

---
+++ task8.py
@@ -0,0 +1,28 @@
+"""Задание 8
+✔️ Три друга взяли вещи в поход. Сформируйте словарь, где ключ — имя друга, а значение —
+кортеж вещей. Ответьте на вопросы:
+✔️ Какие вещи взяли все три друга
+
+✔️ Какие вещи уникальны, есть только у одного друга
+✔️ Какие вещи есть у всех друзей кроме одного
+и имя того, у кого данная вещь отсутствует
+
+✔️ Для решения используйте операции
+с множествами. Код должен расширяться
+на любое большее количество друзей.
+"""
+baggage = {
+    'Denis': ('fishing rod', 'lighter', 'tent'),
+    'Alex': ('tent', 'brazier', 'gun'),
+    'Vlad': ('tent', 'gun', 'sun cream')
+}
+all_items = list(baggage.values())
+must_have_item = set(all_items[0])
+for items in all_items:
+    must_have_item = must_have_item.intersection(set(items))
+print(must_have_item)
+
+uniq_items = {}
+for name, items in baggage.items():
+    uniq_items[name] = set(items).difference(must_have_item)
+print(uniq_items)
